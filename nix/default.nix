42d0e959a67f2219123e21c68137cbc659f09956
David Casier <david.casier@aevoo.fr>
2025-12-06 17:12:53 +0100

    restruct

--- nix/default.nix
+++ nix/default.nix
@@ -5,14 +5,9 @@
 }:
 
 let
-  _modules = pkgs.path + "/nixos/modules";
-  lib = pkgs.lib;
-
   global = import ./global.nix {inherit lib pkgs;};
-  nixunits = pkgs.callPackage ../nixunits.nix {
-    inherit (pkgs) lib stdenv;
-    inherit pkgs;
-  };
+  id = properties.id;
+  lib = pkgs.lib;
 
   modules = [
     (_modules + "/misc/extra-arguments.nix")
@@ -40,32 +35,36 @@ let
       };
     })
   ] else [
-    ({ config, pkgs, lib, ... }: let
-      properties = builtins.fromJSON(propertiesJSON);
-      id = properties.id;
-    in {
+    ({ config, pkgs, lib, ... }: {
       ${global.moduleName}.${id} = import configFile { inherit config lib pkgs properties; };
     })
   ]);
   modules_dir = work_dir + "/modules";
   modules_from_dir =
-  lib.filter (x: x != null)
-    (lib.mapAttrsToList
-      (name: type:
-        if type == "regular" && lib.hasSuffix ".nix" name
-        then modules_dir + "/${name}"
-        else null)
-      (builtins.readDir modules_dir));
+    lib.filter (x: x != null)
+      (lib.mapAttrsToList
+        (name: type:
+          if type == "regular" && lib.hasSuffix ".nix" name
+          then modules_dir + "/${name}"
+          else null)
+        (builtins.readDir modules_dir));
+
+  nixunits = pkgs.callPackage ../nixunits.nix {
+    inherit (pkgs) lib stdenv;
+    inherit pkgs;
+  };
+  properties = builtins.fromJSON(propertiesJSON);
 
   utils = import ./utils.nix;
 
   system = (
     lib.evalModules({
       inherit modules;
-      specialArgs = {inherit pkgs;};
+      specialArgs = { inherit global id pkgs; };
     })
   ).config.system;
   work_dir = "/var/lib/nixunits";
+  _modules = pkgs.path + "/nixos/modules";
 in
   if configFile == null then
     system
